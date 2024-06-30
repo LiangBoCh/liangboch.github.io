be0cd77425bec638e59b157223e24fd70cc9fbd4
梁博超 <14625044+lbch@user.noreply.gitee.com>
2024-06-30 11:37:46 +0800

    Site updated: 2024-06-30 11:37:46

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1719717922057";
+const VERSION = "1719718659947";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
